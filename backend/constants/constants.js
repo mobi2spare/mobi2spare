7ff4ee50eefd12569c15244b1dccf5718d5253b2
Bismeet singh <bismeetsingh352@gmail.com>
2024-07-07 14:48:50 +0530

    constants

--- backend/constants/constants.js
+++ backend/constants/constants.js
@@ -13,11 +13,24 @@ export const ROLES = {
 export const validationMessages = {
     PASSWORD_REQUIRED_MESSAGE: 'Password must be at least 6 characters long',
     PHONE_REQUIRED_MESSAGE: 'Valid Contact Number is required',
-    USERNAME_REQUIRED_MESSAGE: 'A Valid Name is required'
+    USERNAME_REQUIRED_MESSAGE: 'A Valid Name is required',
+    ADHAR_NUMBER_INVALID_MESSAGE: 'A valid adhar number is required'
 }
 
+export const PRODUCT_REQUEST_STATUS = {
+    APPROVED: "Approved",
+    PENDING : "Pending",
+    REJECTED: "Rejected"
 
+}
 
+// export const uploadProductImages = multer({
+
+//     storage : multer.diskStorage({
+
+//         destination
+//     })
+// })
 
 export const upload = multer({
     limits: { fileSize: 1200000 },
@@ -27,7 +40,7 @@ export const upload = multer({
             let userId;
             let uploadPath;
             let subFolder;
-            if (req.params['pid']){
+            if (req.params['uid']){
                 subFolder = 'product';
                 userId = req.params['uid'];
             }
@@ -35,12 +48,13 @@ export const upload = multer({
             {
                 subFolder = 'category';
             }
-            let subFolderId = req.params['pid'] || req.params['categoryId'];
             if (userId != undefined){
-                uploadPath = path.join(process.env.FILE_UPLOAD_PATH, userId,subFolder,subFolderId);
+                // uploads\\userId\\product\\1716034174651-672853170.jpeg
+                uploadPath = path.join(process.env.FILE_UPLOAD_PATH,subFolder);
             }
             else{
-                uploadPath = path.join(process.env.FILE_UPLOAD_PATH,subFolder,subFolderId);
+                // uploads\\category\\categoryid\\1716034174651-672853170.jpeg
+                uploadPath = path.join(process.env.FILE_UPLOAD_PATH,subFolder);
             }
             
             fs.mkdirSync(uploadPath, { recursive: true }); // Create directory if it doesn't exist
